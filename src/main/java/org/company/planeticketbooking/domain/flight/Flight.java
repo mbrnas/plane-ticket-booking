d76b210c37e8cf2312aa106c8db60cef7d2479fd
Matija Brnas <mb8828@rit.edu>
2024-03-06 16:41:32 +0100

    set up flight model

---
+++ src/main/java/org/company/planeticketbooking/domain/flight/Flight.java
@@ -0,0 +1,46 @@
+package org.company.planeticketbooking.domain.flight;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.company.planeticketbooking.domain.airline.Airline;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "flights")
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class Flight {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long flightId;
+
+    private String flightNumber;
+
+    @ManyToOne(fetch=FetchType.LAZY, optional = false)
+    @JoinColumn(name="airline_id", nullable = false)
+    private Airline airline;
+
+    private String origin;
+
+    private String destination;
+
+    @Column(name = "departure_time")
+    private LocalDateTime departureTime;
+
+    @Column(name = "arrival_time")
+    private LocalDateTime arrivalTime;
+
+    private BigDecimal price;
+
+    @Column(name = "seats_total")
+    private Integer seatsTotal;
+
+    @Column(name = "seats_available")
+    private Integer seatsAvailable;
+
+}
