0833b06daa5f89fd861ef935fec6907de21247a1
Matija Brnas <mb8828@rit.edu>
2024-03-06 13:02:11 +0100

    define user and roles

---
+++ src/main/java/org/company/planeticketbooking/domain/ERole.java
@@ -0,0 +1,7 @@
+package org.company.planeticketbooking.domain;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
---
+++ src/main/java/org/company/planeticketbooking/domain/Role.java
@@ -0,0 +1,22 @@
+package org.company.planeticketbooking.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+}
