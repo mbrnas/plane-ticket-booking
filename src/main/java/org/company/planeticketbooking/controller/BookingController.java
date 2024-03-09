3427286b4c77a644472fde2f3acaff317dd62d7f
Matija Brnas <mb8828@rit.edu>
2024-03-09 20:21:36 +0100

    fix delete booking method

--- src/main/java/org/company/planeticketbooking/controller/BookingController.java
+++ src/main/java/org/company/planeticketbooking/controller/BookingController.java
@@ -29,7 +29,7 @@ public class BookingController {
 
     @DeleteMapping("/delete/{id}")
     public void deleteBookingById(@PathVariable Long id) {
-        bookingService.deleteBookingById(id);
+        bookingService.deleteBooking(id);
     }
 
     @PostMapping("/add")
