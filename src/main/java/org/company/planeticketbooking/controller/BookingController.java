29f619ccd34d7e045235aeaa089b5f4e81a40d1f
Matija Brnas <mb8828@rit.edu>
2024-03-13 13:55:07 +0100

    set CORS for booking controller

--- src/main/java/org/company/planeticketbooking/controller/BookingController.java
+++ src/main/java/org/company/planeticketbooking/controller/BookingController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/bookings")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class BookingController {
     private final BookingService bookingService;
 
